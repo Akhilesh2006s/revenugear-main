8ca5ea3be944c934aa339b8b98336a3657232e06
Akhilesh2006s <155376342+Akhilesh2006s@users.noreply.github.com>
2025-06-18 12:53:41 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Car from "../app/Car";
 import GlareCard from "../app/glarecard";
+import Component from "../app/interactive-book"
+
 
 export default function Page() {
   return (
     <>
       <Car />
       <GlareCard />
+      <Component />
     </>
   );
 }
